46f59b54cf030469bfe3c0802947d99bc6578113
Marcelo Trindade <49993899+marcelositr@users.noreply.github.com>
2026-08-29 17:57:54 -0300

    fix(ui): compile exported UI entrypoint

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 fn main() {
     slint_build::compile_with_config(
-        "ui/main.slint",
+        "ui/app.slint",
         slint_build::CompilerConfiguration::new().with_style("fluent".to_owned()),
     )
     .expect("failed to compile Slint UI");
